31b8cff0f0d26a815daa5fb18f4725a4e7bc79ce
Mohammed <mkarim.ali@outlook.com>
2024-04-28 18:42:56 +0200

    first commit

---
+++ StudentsApp.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsApp.Web.Models.Entities;
+
+namespace StudentsApp.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+               
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
